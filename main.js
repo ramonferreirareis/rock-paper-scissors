1b71b50ca68c56ec4451ca76a94b5361819a7a07
Ramon Reis <111209853+ramonferreirareis@users.noreply.github.com>
2024-11-10 22:41:53 -0300

    Add UI to play the game and show score and results

--- main.js
+++ main.js
@@ -38,49 +38,85 @@ function getHumanChoice () {
 
 function playRound(humanChoice, computerChoice) {
    if (humanChoice === computerChoice ) {
-        console.log("It's a tie");
+        resultText.textContent = "It's a tie";
 
    } else if (humanChoice === "rock" && computerChoice === "scissors") {
-        console.log("You've won");
+        resultText.textContent = "You've won";
         humanScore++;
 
    } else if (humanChoice === "rock" && computerChoice === "paper") {
-        console.log("You've lost");
+        resultText.textContent = "You've lost";
         computerScore++;
 
    } else if (humanChoice === "paper" && computerChoice === "rock") {
-        console.log("You've won");
+        resultText.textContent = "You've won";
         humanScore++;
 
    } else if (humanChoice === "paper" && computerChoice === "scissors") {
-        console.log("You've lost");
+        resultText.textContent = "You've lost";
         computerScore++;
 
    } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        console.log("You've won");
+        resultText.textContent = "You've won";
         humanScore++;
 
    } else if (humanChoice === "scissors" && computerChoice === "rock") {
-        console.log("You'lost");
-        computerScore++;   
+        resultText.textContent = "You've lost";        computerScore++;   
    }
+
+   if (humanScore === 5) {
+    alert("You won 5 matches");
+    humanScore = 0;
+    computerScore = 0;
+
+   } else if (computerScore === 5) {
+    alert("You lost 5 matches");
+    humanScore = 0;
+    computerScore = 0;
+}
 
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
-function playGame (numberOfMatches) {
-    for (i = 0; i < numberOfMatches; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection); 
-    }
-    console.log("Your score is: " + humanScore);
-    console.log("The computer score is: " + computerScore);
+// function playGame (numberOfMatches) {
+//     for (i = 0; i < numberOfMatches; i++) {
+//         const humanSelection = getHumanChoice();
+//         const computerSelection = getComputerChoice();
+//         playRound(humanSelection, computerSelection); 
+//     }
+//     console.log("Your score is: " + humanScore);
+//     console.log("The computer score is: " + computerScore);
+// }
+
+const rockButton = document.querySelector("#rock");
+const paperButton = document.querySelector("#paper");
+const scissorsButton = document.querySelector("#scissors");
+const resultText = document.querySelector("#result");
+
+rockButton.addEventListener("click", () => {
+    playRound("rock", getComputerChoice());
+    showScore();
+});
+paperButton.addEventListener("click", () => {
+    playRound("paper", getComputerChoice())
+    showScore();
+});
+scissorsButton.addEventListener("click", () => {
+    playRound("scissors", getComputerChoice())
+    showScore();
+});
+
+const humanScoreDisplay = document.querySelector("#humanScore");
+const computerScoreDisplay = document.querySelector("#computerScore");
+
+function showScore() {
+    humanScoreDisplay.textContent = "Your score: " + humanScore;
+    computerScoreDisplay.textContent = "Computer score: " + computerScore;
 }
 
-playGame(10);
+
 
 
 
